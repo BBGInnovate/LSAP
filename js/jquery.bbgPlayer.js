919470922c92f4e3928da2309a61c7b1c93578cb
hectorportillo <loveuxdesign@gmail.com>
2013-11-06 19:31:58 +0000

    tracking content pull bug on older devices

--- js/jquery.bbgPlayer.js
+++ js/jquery.bbgPlayer.js
@@ -200,8 +200,6 @@
 				url: configUrl,
 				dataType: 'xml',
 				success: function(xml) {
-					console.log( $(xml).find("menu") );
-					
 					configMenuXml = $(xml).find("menu");
 					parseStyles($(xml).find("styles"));
 					parseConfig($(xml).find("config"));
@@ -434,9 +432,7 @@
 		 */
 		function parseStyles(xml) {
 			var styleUrl = '';
-			console.log( 'test' );
 			xml.find("stylesheet").each(function(i) {
-				console.log( $(this).text() );
 				styleUrl = self.config.styleFolder + $(this).text();
 				$('head').append('<link rel="stylesheet" href="' + styleUrl + '">');
 			});
